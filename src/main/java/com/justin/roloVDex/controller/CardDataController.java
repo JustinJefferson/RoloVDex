89abd1b4c0756e2d336fc68c86e107efb0ab4cf6
Justin <justleland@gmail.com>
2019-08-13 13:08:49 -0400

    allowed Cross Origin Resource sharing for local development will need to be implemented at a general config class

--- src/main/java/com/justin/roloVDex/controller/CardDataController.java
+++ src/main/java/com/justin/roloVDex/controller/CardDataController.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 @Repository
 @RequestMapping("/api/carddata")
+@CrossOrigin(origins = "http://localhost:4200")
 public class CardDataController {
 
     private CardDataService service;
